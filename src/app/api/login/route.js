ea7e0d4f93fcf2b0321841a9fe412eedb5510477
Mayur <shelkemayur4700@gmail.com>
2023-10-02 22:27:56 +0530

    login api completed

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,30 +1,53 @@
 import { NextResponse } from "next/server";
 import { User } from "@/models/user";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function POST(request) {
-  console.log("login");
+  console.log("login api ---");
   const { email, password } = await request.json();
 
   try {
-    // 1.GET USER 
+    // 1.GET USER
     const user = await User.findOne({
       email: email,
+      // password: password,
     });
-    console.log(user);
     if (user == null) {
       throw new Error("user not found !");
     }
-    // 2. PASSWORD CHECK 
+    // 2. PASSWORD CHECK
     const matched = bcrypt.compareSync(password, user.password);
     if (!matched) {
       throw new Error("Password not matched");
     }
-    return NextResponse.json({
-      message: "logged in",
+
+    // 3. GENERATE TOKEN
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        name: user.name,
+      },
+      process.env.JWT_KEY
+    );
+    console.log("here is token ", token);
+
+    // 4. SETTING TOKEN IN COOKIE
+    const response = NextResponse.json({
+      message: "success",
+      success: true,
     });
+
+    response.cookies.set("authToken", token, {
+      expiresIn: "1d",
+      httpOnly: true,
+    });
+
+    console.log(user);
+    return response;
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     return NextResponse.json(
       {
         message: error.message,
