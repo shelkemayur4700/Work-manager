0e760b0f54a2263e0ab32d4ebb17d6753eb81272
Mayur <shelkemayur4700@gmail.com>
2023-10-06 12:44:47 +0530

    fixed current api issue

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -4,17 +4,17 @@ import { User } from "@/models/user";
 import { connectDb } from "@/helper/db";
 
 export async function GET(request) {
-  try {
-    const authToken = request.cookies.get("authToken")?.value;
-    // console.log("token from cookies", authToken);
-    const data = jwt.verify(authToken, process.env.JWT_KEY);
-    // console.log(data);
-    await connectDb();
-    const user = await User.findById(data._id).select("-password");
-    // console.log("user from current API", user);
-    return NextResponse.json(user);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.error("Internal server error", { status: 500 });
+  const authToken = request.cookies.get("authToken")?.value;
+
+  // console.log("token from cookies", authToken);
+  if (!authToken) {
+    return NextResponse.error("User is not logged in !!");
   }
+
+  const data = jwt.verify(authToken, process.env.JWT_KEY);
+  // console.log(data);
+  await connectDb();
+  const user = await User.findById(data._id).select("-password");
+  // console.log("user from current API", user);
+  return NextResponse.json(user);
 }
