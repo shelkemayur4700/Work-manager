c452ce5d53cbf0d25d5a53bbec986d5de0446087
Mayur <shelkemayur4700@gmail.com>
2023-10-06 10:47:13 +0530

    redeploy1

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,16 +1,20 @@
 import { NextResponse } from "next/server";
-import { toast } from "react-toastify";
 import jwt from "jsonwebtoken";
 import { User } from "@/models/user";
 import { connectDb } from "@/helper/db";
 
 export async function GET(request) {
-  const authToken = request.cookies.get("authToken")?.value;
-  // console.log("token from cookies", authToken);
-  const data = jwt.verify(authToken, process.env.JWT_KEY);
-  console.log(data);
-  await connectDb();
-  const user = await User.findById(data._id).select("-password");
-  console.log("user from current API", user);
-  return NextResponse.json(user);
+  try {
+    const authToken = request.cookies.get("authToken")?.value;
+    // console.log("token from cookies", authToken);
+    const data = jwt.verify(authToken, process.env.JWT_KEY);
+    // console.log(data);
+    await connectDb();
+    const user = await User.findById(data._id).select("-password");
+    // console.log("user from current API", user);
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error("Internal server error", { status: 500 });
+  }
 }
